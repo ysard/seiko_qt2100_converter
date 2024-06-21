f163f08b04a47f05ba111b821d5b2158ea601acd
ysard <ysard@users.noreply.github.com>
2024-06-21 02:00:30 +0200

    Add logger

---
+++ seiko_converter/commons.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+#  Seiko Converter is a software allowing to generate graphs based on the raw
+#  data produced by the Seiko Qt-2100 Timegrapher device.
+#  Copyright (C) 2024  Ysard
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Logger settings and project constants"""
+# Standard imports
+from logging.handlers import RotatingFileHandler
+import logging
+
+
+# Logging
+LOGGER_NAME = "seiko-converter"
+LOG_LEVEL = "DEBUG"  # TODO marche pas dans les plugins ...
+LOG_LEVELS = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "notset": logging.NOTSET,
+}
+
+################################################################################
+
+
+def logger(name=LOGGER_NAME):
+    """Return logger of given name, without initialize it.
+
+    Equivalent of logging.getLogger() call.
+    """
+    logger_obj = logging.getLogger(name)
+    fmt_str = "%(levelname)s: [%(filename)s:%(lineno)s:%(funcName)s()] %(message)s"
+    logging.basicConfig(format=fmt_str)
+    return logger_obj
+
+
+_logger = logging.getLogger(LOGGER_NAME)
+_logger.setLevel(LOG_LEVEL)
+
+def log_level(level):
+    """Set terminal/file log level to the given one.
+
+    .. note:: Don't forget the propagation system of messages:
+        From logger to handlers. Handlers receive log messages only if
+        the main logger doesn't filter them.
+    """
+    # Main logger
+    _logger.setLevel(level.upper())
+    # Handlers
+    [
+        handler.setLevel(level.upper())
+        for handler in _logger.handlers
+        if handler.__class__
+        in (logging.StreamHandler, logging.handlers.RotatingFileHandler)
+    ]
